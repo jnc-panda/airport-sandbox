8122a253aec7d41314046d43e15972bc51b41c7b
Joel Cohen <jcohenf50@gmail.com>
2024-10-23 16:07:49 +0100

    feat: added Aeroplane, Helicopter and Vehicle classes

---
+++ src/main/java/Aeroplane.java
@@ -0,0 +1,14 @@
+public class Aeroplane {
+
+    int capacity;
+    boolean hasCrew;
+    String destination;
+
+    public Aeroplane(String destination, boolean hasCrew, int capacity) {
+        this.destination = destination;
+        this.hasCrew = hasCrew;
+        this.capacity = capacity;
+    }
+
+
+}
